c79c37d22b0124d3abb606a0fd069b88921cfd31
sjkim <sjkim@DESKTOP-FGNOKCR>
2020-10-06 17:40:32 +0900

    20201006_react 11장 완료

--- todo-app/src/App_reducer.js
+++ todo-app/src/App_reducer.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback }from 'react';
+import React, { useReducer, useRef, useCallback }from 'react';
 import TodoTemplate from './components/TodoTemplate'
 import TodoInsert from './components/TodoInsert'
 import TodoList from './components/TodoList'
@@ -17,12 +17,27 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT' :   // 추가
+      return todos.concat(action.todo);
+    case 'REMOVE' :   // 삭제
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE' : 
+      return todos.map(todo =>
+        todo.id === action.id ? {...todo, checked : !todo.checked} : todo);
+    default :
+      return todos;
+  }
+}
+
 const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+  // const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   // 고유값으로 사용할 id
   // ref를 사용하여 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   const onInsert = useCallback((text) => {
     const todo = {
@@ -30,27 +45,17 @@ const App = () => {
       , text
       , checked : false
     };
-    // 최적화 위한 수정
-    // setTodos(todos.concat(todo));
-    setTodos(todos => todos.concat(todo));
+    dispatch({type : 'INSERT', todo});
     nextId.current += 1;  // 1씩 더하기
   }, /*[todos]*/ [])   // 두번째 파라미터가 빈 배열이 아닐 경우
                 // 해당 값이 변경될 경우에만 렌더링
 
   const onRemove = useCallback((id) => {
-    // 최적화 위한 수정
-    // setTodos(todos.filter(todo => todo.id !== id))
-    setTodos(todos => todos.filter(todo => todo.id !== id))
+    dispatch({type : 'REMOVE', id});
   }, /*[todos]*/ [])
 
   const onToggle = useCallback((id) => {
-    // 최적화 위한 수정
-    // setTodos(todos.map(todo => 
-    //   todo.id === id ? {...todo, checked: !todo.checked } : todo
-    setTodos(todos => 
-      todos.map(todo => 
-        todo.id === id ? {...todo, checked: !todo.checked } : todo
-    ))
+    dispatch({type : 'TOGGLE', id});
   }, /*[todos]*/ []);
 
   return (
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -7,19 +7,26 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss'
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
     const {id, text, checked} = todo;
     return (
-        <div className="TodoListItem">
-            <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-                <div className="text">{text}</div>
-            </div>
-            <div className="remove" onClick={() => onRemove(id)}>
-                <MdRemoveCircleOutline />
+        <div class="TodoListItem-virtualized" style={style}>
+            <div className="TodoListItem">
+                <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+                    {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                    <div className="text">{text}</div>
+                </div>
+                <div className="remove" onClick={() => onRemove(id)}>
+                    <MdRemoveCircleOutline />
+                </div>
             </div>
         </div>
     );
 };
 
-export default TodoListItem;
+// React.memo 사용하여 컴포넌트 리렌더링 방지
+// todo, onRemove, onToggle 이 바뀌지 않으면 리렌더링안함
+export default React.memo(
+    TodoListItem
+    , (prevProps, nextProps) => prevProps.todo === nextProps.todo
+);
