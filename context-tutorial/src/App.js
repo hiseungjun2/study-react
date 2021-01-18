17b6685d0e96faf10996abd1c3dba36dc27574cb
sjkim <hiseungjun2@icloud.com>
2021-01-18 22:55:46 +0900

    20210118_15장 완료

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import ColorBox from './components/ColorBox';
 import ColorContext from './contexts/color';
 import { ColorProvider } from './contexts/color'
+import SelectColors from './components/SelectColors';
 
 function App() {
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox/>
       </div>
     </ColorProvider>
--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ColorContext from '../contexts/color'
 import { ColorConsumer } from '../contexts/color'
 
 const ColorBox = () => {
+    const { state } = useContext(ColorContext);
+
     return (
-        <ColorConsumer>
-            {value => (
-                <>
-                    <div
-                        style={{
-                            width : '64px',
-                            height : '64px',
-                            background : value.state.color
-                        }}
-                    />
-                    <div
-                        style={{
-                            width : '32px',
-                            height : '32px',
-                            background : value.state.subcolor
-                        }}
-                    />
-                </>
-            )}
-        </ColorConsumer>
+        // useContext Hook 사용 (클래스형 컴포넌트에서는 사용 x)
+        <>
+            <div 
+                style={{
+                    width : '64px',
+                    height : '64px',
+                    backgroud : state.color
+                }}
+            />
+            <div
+                style={{
+                    width : '32px',
+                    height : '32px',
+                    background : state.subcolor
+                }}
+            />
+        </>
+
+        // <ColorConsumer>
+        //     {value => (
+        //         <>
+        //             <div
+        //                 style={{
+        //                     width : '64px',
+        //                     height : '64px',
+        //                     background : value.state.color
+        //                 }}
+        //             />
+        //             <div
+        //                 style={{
+        //                     width : '32px',
+        //                     height : '32px',
+        //                     background : value.state.subcolor
+        //                 }}
+        //             />
+        //         </>
+        //     )}
+        // </ColorConsumer>
+
         // <ColorContext.Consumer>
         //     {value => (
         //         <div
