b97ddbb7ac3c1033f55466b09ad94eb80b473e97
sjkim <hiseungjun2@icloud.com>
2021-02-25 22:47:09 +0900

    20210225_24장 styled-components 추가 및 css 수정

---
+++ blog/blog-frontend/src/components/common/Button.js
@@ -0,0 +1,25 @@
+// 공통 버튼 스타일링
+
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+
+const StyledButton = styled.button`
+    border : none;
+    border-radius : 4px;
+    font-size : 1rem;
+    font-weight : bold;
+    padding : 0.25rem 1rem;
+    color : white;
+    outline : none;
+    cursor : pointer;
+
+    background : ${palette.gray[8]};
+    &:hover {
+        background : ${palette.gray[6]};
+    }
+`;
+
+const Button = props => <StyledButton {...props} />;
+
+export default Button;
