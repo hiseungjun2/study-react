e6f72c77106b78089a75dc14aa20467d96fe8ae0
sjkim <hiseungjun2@icloud.com>
2021-03-03 22:07:58 +0900

    20210303_24장 회원가입 구현

--- blog/blog-frontend/src/modules/auth.js
+++ blog/blog-frontend/src/modules/auth.js
@@ -1,8 +1,14 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import { takeLatest } from 'redux-saga/effects';
+import createRequestSaga, { createRequestActionType } from '../lib/createRequestSaga';
+import * as authAPI from '../lib/api/auth';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+const [ REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE ] = createRequestActionType('auth/REGISTER');
+const [ LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE ] = createRequestActionType('auth/LOGIN');
 
 export const changeField = createAction(
     CHANGE_FIELD,
@@ -15,6 +21,23 @@ export const changeField = createAction(
 
 export const initializeForm = createAction(INITIALIZE_FORM, form => form);      // register / login
 
+export const register = createAction(REGISTER, ({ username, password }) => ({
+    username,
+    password
+}));
+export const login = createAction(LOGIN, ({ username, password }) => ({
+    username,
+    password
+}));
+
+// 사가 생성
+const registerSaga = createRequestSaga(REGISTER, authAPI.register);
+const loginSaga = createRequestSaga(LOGIN, authAPI.login);
+export function* authSaga() {
+    yield takeLatest(REGISTER, registerSaga);
+    yield takeLatest(LOGIN, loginSaga);
+}
+
 const initialState = {
     register : {
         username : '',
@@ -24,18 +47,43 @@ const initialState = {
     login : {
         username : '',
         password : '',
-    }
+    },
+    auth : null,
+    authError : null
 };
 
 const auth = handleActions(
     {
-        [CHANGE_FIELD] : (state, { payload : {form, key, value } }) => 
+        [CHANGE_FIELD] : (state, { payload : { form, key, value } }) => 
         produce(state, draft => {
             draft[form][key] = value;   // ex) state.register.username 을 바꾼다
         }),
         [INITIALIZE_FORM] : (state, { payload : form }) => ({
             ...state,
             [form] : initialState[form],
+            authError : null,   // ex) state.register.username 을 바꾼다
+        }),
+        // 회원가입 성공
+        [REGISTER_SUCCESS] : (state, { payload : auth }) => ({
+            ...state,
+            authError : null,
+            auth
+        }),
+        // 회원가입 실패
+        [REGISTER_FAILURE] : (state, { payload : error }) => ({
+            ...state,
+            authError : error,
+        }),
+        // 로그인 성공
+        [LOGIN_SUCCESS] : (state, { payload : auth }) => ({
+            ...state,
+            authError : null,
+            auth,
+        }),
+        // 로그인 실패
+        [LOGIN_FAILURE] : (state, { payload : error }) => ({
+            ...state,
+            authError : error,
         })
     },
     initialState
